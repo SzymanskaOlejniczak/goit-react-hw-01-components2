0f8c923e5cf5050cdbc578323fe84117b0d6ad54
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-12-14 20:57:25 +0100

    Create FriendListItem.jsx

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import styles from './FriendListItem.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline}) => {
+  return (
+    <li className={styles.item}>
+      <span className={isOnline ? styles.statusOnline : styles.statusOffline}>{isOnline}</span>
+      <img className="avatar" src={avatar} alt={name} width="70" />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  isOnline: PropTypes.bool,
+};
+export default FriendListItem
